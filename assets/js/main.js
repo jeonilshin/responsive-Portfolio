b05cdc33c342f2d879bb735b12629264eb287321
jeonilshin <jeonilshinbusiness@gmail.com>
2023-08-21 01:30:28 +0900

    footer added

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,7 +40,27 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_fsjvo5f','template_rgmgn7m','#contact-form','bipREOg7PXnRrX-ja')
+    .then(() => {
+        contactMessage.textContent = 'Message sent successfully'
+
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+    }, () => {
+        contactMessage.textContent = 'Message not sent (service error)'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
