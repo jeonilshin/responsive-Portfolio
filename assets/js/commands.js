208f0d47f319e37e1ddc0a2a8711b4fd4cd6d10b
jeonilshin <jeonilshinbusiness@gmail.com>
2023-08-30 18:39:02 +0900

    finalized

---
+++ assets/js/commands.js
@@ -0,0 +1,222 @@
+const LINKEDIN = "https://www.linkedin.com/in/ilshin-jeon-4011b6235/";
+const INSTAGRAM = "https://www.instagram.com/jeon.ilshin/";
+const GITHUB = "https://github.com/jeonilshin";
+const EMAIL = 'cloud.jeonilshin@gmail.com';
+const USERNAME = "jeonilshin";
+
+const ABOUT = [
+  "<br>",
+  "Hello there!👋" ,
+  "              ",
+  "I'm Shin, a Student, Studying ",
+  "hard to be a Cloud Engineer.",
+  "I love cats and to code. 🧑‍💻",
+  "                            ",
+  "                            ",
+  "I have always been interested in",
+  "computer since I was a middle school",
+  "student. I always wanted to be a developer",
+  "It was always my dream to make an app to",
+  "make people happy and to make their lives",
+  "easier.",
+  "                                     ",
+  "As I approach my final year of ",
+  "highschool, I'm eager to explore job ",
+  "opportunities and dive deeper into", 
+  "research projects. It is now my" ,
+  "time to show my skills and to be",
+  " a full time Full Stack Engineer. 🚀",,
+  "<br>"
+];
+
+
+
+SOCIAL = [
+  "<br>",
+  'linkedin       <a href="' + LINKEDIN + '" target="_blank">linkedin/' + USERNAME + "</a>",
+  'instagram      <a href="' + INSTAGRAM + '" target="_blank">instagram/' + USERNAME + '</a>',
+  'github         <a href="' + GITHUB + '" target="_blank">github/' + USERNAME + "</a>",
+  "<br>"
+];
+
+
+PROJECTS = [
+  "<br>",
+  "Here are some of my projects:",
+  "<br>",
+  "<strong> - Website <strong>",
+  "Responsive Portfolio Website",
+  "it has clean and modern design",
+  "and has animation with responsive features",
+  "<a href=https://github.com/jeonilshin/responsive-Portfolio> GitHub link",
+  "Tech stack: HTML, CSS, JavaScript",
+  "<br>",
+  "<strong> - Website <strong>",
+  "Website with Next.JS",
+  "website made with Next.JS, it has",
+  "high level design with animation and",
+  "responsive features",
+  "Tech stack: Next JS, Tailwind CSS, JavaScript",
+  "<a href=https://github.com/jeonilshin/portfolio-with-Next.JS> GitHub link",
+  "<br>",
+  "<strong> - Web App <strong>",
+  "Dropbox with GoLang",
+  "A cloud storage app like Dropbox", 
+  "that you can upload and save it",
+  "to your AWS S3 Bucket",
+  "<a href=https://github.com/jeonilshin/dropbox-with-golang> GitHub link",
+  "<br>",
+  "<strong> More at my GitHub <strong>",
+  "<br>"
+
+];
+
+COMMAND_LIST = [
+  "<br>",
+  '<span class="command">about</span>          Learn more about me',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">education</span>      Show my education',
+  '<span class="command">notion</span>         Open my web curriculum',
+  '<span class="command">projects</span>       View some of my projects',
+  '<span class="command">history</span>        Show command history',
+  '<span class="command">email</span>          Feel free to reach out',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">echo</span>           Print any give string',
+  '<span class="command">ls</span>             List directory content',
+  '<span class="command">cd &lt;directory&gt</span> Change directory',
+  '<span class="command">help</span>           Display all commands',
+  '<span class="command">cat</span>            Print file content',
+  "<br>",
+];
+
+BANNER = [
+  "                                  ® 2023   ",
+  "░██████╗ ██╗░░██╗ ██╗ ███╗░░██╗██╗░██████╗ ",
+  "██╔════╝ ██║░░██║ ██║ ████╗░██║╚█║██╔════╝ ",
+  "╚█████╗░ ███████║ ██║ ██╔██╗██║░╚╝╚█████╗░ ",
+  "░╚═══██╗ ██╔══██║ ██║ ██║╚████║░░░░╚═══██╗ ",
+  "██████╔╝ ██║░░██║ ██║ ██║░╚███║░░░██████╔╝ ",
+  "╚═════╝░ ╚═╝░░╚═╝ ╚═╝ ╚═╝░░╚══╝░░░╚═════╝░ ",
+  "      ▀█▀ █▀▀ █▀▄ █▄█ ▀█▀ █▀█ █▀█ █        ",
+  "       █  █▀▀ █▀▄ █ █  █  █ █ █▀█ █        ",
+  "       ▀  ▀▀▀ ▀ ▀ ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀ ▀▀▀      ",
+  ]
+
+DIRECTORIES = [ 
+"<span class='directory'>github/</span>",
+"mystery",
+"<span class='directory'>webPortfolio/</span>",
+]
+
+EMAIL_INFO = [
+  "<a href=mailto:" + EMAIL + ">" + EMAIL +"</a>",
+]
+
+TOUCH = [
+  "You are not an administrator."
+]
+
+SUDO = [
+  "You are not an administrator."
+]
+
+VI = [
+  "You are not an administrator."
+]
+
+RM = [
+  "You are not an administrator."
+]
+
+EMPTY = []
+
+EDUCATION_INFO = [
+  "|",
+  "<strong>Stella Maris Academy of Davao</strong>",
+  "2011. 6 - 2015. 5",
+  "|",
+  "<strong>St. Paul College of Davao</strong>",
+  "2015. 6 - 2017. 5",
+  "Graduated (졸업)",
+  "|",
+  "<strong>부산다문화국제학교</strong> (Busan Multicultural International School)",
+  "2017. 7 - 2018. 2",
+  "|",
+  "<strong>영도제일중학교</strong> (Yeongdo Jeil Middle School)",
+  "2018. 3 - 2021. 2",
+  "Graduated (졸업)",
+  "|",
+  "<strong>대양고등학교</strong> (Daeyang High School)",
+  "2021. 3 - 2024. 2",
+  "To be Graduated (졸업 예정)",
+  "|",
+]
+
+MOBILE_EDUCATION_INFO = [
+  "|",
+  "<strong>Stella Maris Academy of Davao</strong>",
+  "2011. 6 - 2015. 5",
+  "|",
+  "<strong>St. Paul College of Davao</strong>",
+  "2015. 6 - 2017. 5",
+  "Graduated (졸업)",
+  "|",
+  "<strong>부산다문화국제학교</strong> (Busan Multicultural International School)",
+  "2017. 7 - 2018. 2",
+  "|",
+  "<strong>영도제일중학교</strong> (Yeongdo Jeil Middle School)",
+  "2018. 3 - 2021. 2",
+  "Graduated (졸업)",
+  "|",
+  "<strong>대양고등학교</strong> (Daeyang High School)",
+  "2021. 3 - 2024. 2",
+  "To be Graduated (졸업 예정)",
+  "|",
+]
+
+
+TERMINAL_INFO = [
+"                            ",
+'Hi! Welcome to my Terminal!',
+"--------",
+"                            ",
+"I'm Studying to be a...",
+"--------",
+" * Cloud Engineer ",
+" * DevOps Engineer ",
+" * Full-Stack Engineer ",
+"--------",
+"                            ",
+"* This terminal is new, so I still haven't added the <span class=\"command\">'TAB TO COMPLETE'</span> feature.",
+"--------",
+"                            ",
+"For a list of available commands, type <span class=\"command\">'help'</span>.",
+"                            ",
+]
+
+TERMINAL_INFO_MOBILE = [
+  'Hi! Welcome to my Terminal!',
+  "--------",
+  "I'm Studying to be a...",
+  "--------",
+  " * Cloud Engineer ",
+  " * DevOps Engineer ",
+  " * Full-Stack Engineer ",
+  "---------",
+  "For a list of available commands, type <span class=\"command\">'help'</span>.",
+  "                            ",
+  ]
+
+MOBILE_BANNER = [
+  "                                  ® 2023   ",
+  "░██████╗ ██╗░░██╗ ██╗ ███╗░░██╗██╗░██████╗ ",
+  "██╔════╝ ██║░░██║ ██║ ████╗░██║╚█║██╔════╝ ",
+  "╚█████╗░ ███████║ ██║ ██╔██╗██║░╚╝╚█████╗░ ",
+  "░╚═══██╗ ██╔══██║ ██║ ██║╚████║░░░░╚═══██╗ ",
+  "██████╔╝ ██║░░██║ ██║ ██║░╚███║░░░██████╔╝ ",
+  "╚═════╝░ ╚═╝░░╚═╝ ╚═╝ ╚═╝░░╚══╝░░░╚═════╝░ ",
+  "      ▀█▀ █▀▀ █▀▄ █▄█ ▀█▀ █▀█ █▀█ █        ",
+  "       █  █▀▀ █▀▄ █ █  █  █ █ █▀█ █        ",
+  "       ▀  ▀▀▀ ▀ ▀ ▀ ▀ ▀▀▀ ▀ ▀ ▀ ▀ ▀▀▀      ",
+  ]
